7c883f7f43d599be65a4499c11d5b404577ceea3
dudarenkoff <dudarenkoff@gmail.com>
2022-07-11 15:01:46 +0300

    Adding dist

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/fingerprint-on-vue-3/',
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
